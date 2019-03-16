f57429fa48ffa41fe81943d4b72d96d8c335248e
Mohd Jalaluddin Ahmad <9w2tja@gmail.com>
2019-03-16 12:05:45 +0800

    Update Task 3.cpp
    update logic error

--- Selection Structures/Task 3.cpp
+++ Selection Structures/Task 3.cpp
@@ -6,13 +6,13 @@ int main(void){
 	printf("Input the Grade Point Average : ");
 	scanf("%f", &gpa);
 	
-	if (gpa <= 0.99)
+	if (gpa < 1.0)
 		printf("Failed semester—registration suspended");
-	else if (gpa <= 1.99)
+	else if (gpa < 2.0)
 		printf("On probation for next semester");
-	else if (gpa <= 2.99)
+	else if (gpa < 3.0)
 		printf("(no message)");
-	else if (gpa <= 3.49)
+	else if (gpa < 3.5)
 		printf("Dean’s list for semester");
 	else if (gpa <= 4.00)
 		printf("Highest honors for semester");
